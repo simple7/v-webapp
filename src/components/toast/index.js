693e1416fafbfa7ab08b7d459d137d61a5526b2a
simple7 <18352538401@163.com>
2017-05-25 18:19:23 +0800

    toast commit

---
+++ src/components/toast/index.js
@@ -0,0 +1,14 @@
+import { initPlugin } from './js/toast';
+
+const Toasted = {
+    install: (Vue, options) => {
+        initPlugin(Vue, options)
+    }
+};
+
+// register plugin if it is used via cdn or directly as a script tag
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Toasted = Toasted;
+}
+
+export default Toasted
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import toast from './components/toast/index'
 
 Vue.config.productionTip = false
 
+Vue.use(toast);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
